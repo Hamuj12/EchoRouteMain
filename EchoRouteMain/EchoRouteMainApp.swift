f57f7e5d12196329f1a7633c4701b564f25a8139
Hamuj12 <hamza.mujtaba@utexas.edu>
2024-10-03 13:13:03 -0500

    Initial Commit

---
+++ EchoRouteMain/EchoRouteMainApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoRouteMainApp.swift
+//  EchoRouteMain
+//
+//  Created by Hamza Mujtaba on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct EchoRouteMainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EchoRouteMainTests/EchoRouteMainTests.swift
@@ -0,0 +1,17 @@
+//
+//  EchoRouteMainTests.swift
+//  EchoRouteMainTests
+//
+//  Created by Hamza Mujtaba on 10/3/24.
+//
+
+import Testing
+@testable import EchoRouteMain
+
+struct EchoRouteMainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
